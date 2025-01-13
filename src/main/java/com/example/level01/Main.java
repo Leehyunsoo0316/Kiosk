337cc7b53c52658cbf221acff70ddac20eb1514f
이현수 <mytime0316@naver.com>
2025-01-13 20:54:41 +0900

    Lv1 기본 키오스크 구현

--- src/main/java/com/example/level01/Main.java
+++ src/main/java/com/example/level01/Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
             System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
             System.out.println("0. 종료      | 종료");
-            System.out.println("번호를 입력하세요.");
+            System.out.print("번호를 입력하세요.: ");
             int number = sc.nextInt();
 
             if (number == 1) {
